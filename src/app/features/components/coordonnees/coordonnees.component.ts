ec2f29039466a9dad5a069b2126a3122021f0270
Lucas Schiavetti <lucassc@live.fr>
2025-03-31 14:24:09 +0200

    Commit

---
+++ src/app/features/components/coordonnees/coordonnees.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-coordonnees',
+  standalone: true,
+  imports: [],
+  templateUrl: './coordonnees.component.html',
+  styleUrl: './coordonnees.component.scss'
+})
+export class CoordonneesComponent {
+
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,23 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideRouter, RouterModule } from '@angular/router';
+import { InformationsComponent } from './app/features/components/informations/informations.component';
+import { DiplomesComponent } from './app/features/components/diplomes/diplomes.component';
+import { CompetencesComponent } from './app/features/components/competences/competences.component';
+import { ProjetsComponent } from './app/features/components/projets/projets.component';
+import { ExperienceComponent } from './app/features/components/experience/experience.component';
+import { CoordonneesComponent } from './app/features/components/coordonnees/coordonnees.component';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter([
+      { path: '', component: InformationsComponent },
+      { path: 'diplomes', component: DiplomesComponent },
+      { path: 'competences', component: CompetencesComponent },
+      { path: 'projets', component: ProjetsComponent },
+      { path: 'experience', component: ExperienceComponent },
+      { path: 'coordonnees', component: CoordonneesComponent },
+      { path: '**', redirectTo: '' },
+    ])
+  ]
+});
